cc5ff323b11fdc00dc696d1aac5f708ba5c4855d
DanSytnikov <dan.sytnikov@gmail.com>
2020-03-15 01:15:07 +0200

    Design

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const ejs = require('ejs')
 
 
 const password = '12345';
+const adminPassword = 'admin';
+let isAdmin = false;
 const members = []
 
 
@@ -14,36 +16,44 @@ bodyParser.json()
 app.use('/scripts', express.static(__dirname + '/scripts'));
 app.set("view engine", "ejs");
 
-app.get('/', function (req, res) {
-  res.redirect('authorization')
+app.get('/', function(req, res) {
+    res.redirect('authorization')
 })
-app.get('/admin', (req, res)=>{
-  res.render('admin')
+app.get('/admin', (req, res) => {
+    res.render('admin')
 })
-app.get('/authorization' ,(req, res)=>{
-  res.render('authorization')
+app.get('/authorization', (req, res) => {
+    res.render('authorization')
 })
 
-app.post('/admin', urlencodedJson, function(req,res, next){
-  req.body.forEach(member => members.push(member));
-  console.log(members);
-  
-  res.end();
+app.post('/admin', urlencodedJson, function(req, res, next) {
+    req.body.forEach(member => members.push(member));
+    console.log(members);
+
+    res.end();
 })
 
 app.post('/vote', urlencodedParser, function(req, res, next) {
-    if (password === req.body.password) {
-      res.render('vote', {members: members})
-    } else {
-      res.send('Пшол Вон')
-    }
 
+    switch (req.body.password) {
+        case adminPassword:
+            isAdmin = true;
+            res.render('vote', { members: members });
+            break;
+        case password:
+            res.render('vote', { members: members })
+            break;
+        default:
+            res.render('authorization');
+            break;
+    }
     res.end();
 });
 
-app.post('/result', urlencodedParser, function (req, res, next) {
-  console.log(req.body);
-  res.end();
+app.post('/result', urlencodedParser, function(req, res, next) {
+    console.log(req.body);
+    res.render('result')
+    res.end();
 })
 
 
