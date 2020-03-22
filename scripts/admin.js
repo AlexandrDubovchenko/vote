de05fb18221812cc64a8048fc3145d5e9f2ee493
DanSytnikov <dan.sytnikov@gmail.com>
2020-03-22 21:40:09 +0200

    styles

--- scripts/admin.js
+++ scripts/admin.js
@@ -9,70 +9,74 @@ const members = [];
 $btnExit.addEventListener('click', () => {
     deleteCookie('passwordAdmin');
     $exitLink.click();
-  });
+});
 
 
-$formAdmin.addEventListener('submit', (e)=>{   
+$formAdmin.addEventListener('submit', (e) => {
     e.preventDefault()
     createVote($formAdmin, members);
 });
-$add.addEventListener('click', ()=>{
+$add.addEventListener('click', () => {
     addMember($formAdmin, members)
-    document.querySelector('[name="member"]').value='';      
+    document.querySelector('[name="member"]').value = '';
 });
 
 
-function render(parent, content){
+function render(parent, content) {
     const $li = document.createElement('li');
-    $li.textContent =  content;
+    const $rbtn = document.createElement('button');
+    $rbtn.className = 'btn-circle';
+    $rbtn.textContent = "✘";
+    $li.textContent = content;
     parent.append($li);
+    $li.appendChild($rbtn);
 }
 
-function addMember($formAdmin, members){
+function addMember($formAdmin, members) {
     formData = new FormData($formAdmin);
-    const memberInput =  formDataToObj(formData).member;
-        if (memberInput.split(' ').filter((el)=> el !== '').length > 0 ){
-            members.push(memberInput)
-            render(document.querySelector('.members'), memberInput);
-        }
+    const memberInput = formDataToObj(formData).member;
+    if (memberInput.split(' ').filter((el) => el !== '').length > 0) {
+        members.push(memberInput)
+        render(document.querySelector('.members'), memberInput);
+    }
 
 
-    };
+};
 
-function createVote($formAdmin, members){
+function createVote($formAdmin, members) {
     if (members.length > 0) {
         formData = new FormData($formAdmin)
-    
-    if (getCookie('passwordAdmin') === formDataToObj(formData).password) {
-        alert('Пароль не должен совпадать с паролем админа');
-        return
-    };
-    const password = formDataToObj(formData).password;
-    const appt = formDataToObj(formData).appt;
-    const deadline = {
-        minutes : +appt.split(':')[0],
-        seconds : +appt.split(':')[1],
-        get deadlineSeconds() {
-            return this.minutes*60 + this.seconds;
-        },
-    };
-    const adminData = {
-        members: members,
-        timeStart: +moment().format('X'),
-        password: password,
-        deadline : deadline.deadlineSeconds 
-        
+
+        if (getCookie('passwordAdmin') === formDataToObj(formData).password) {
+            alert('Пароль не должен совпадать с паролем админа');
+            return
+        };
+        const password = formDataToObj(formData).password;
+        const appt = formDataToObj(formData).appt;
+        const deadline = {
+            minutes: +appt.split(':')[0],
+            seconds: +appt.split(':')[1],
+            get deadlineSeconds() {
+                return this.minutes * 60 + this.seconds;
+            },
+        };
+        const adminData = {
+            members: members,
+            timeStart: +moment().format('X'),
+            password: password,
+            deadline: deadline.deadlineSeconds
+
+        }
+
+        fetch('/create', {
+            method: 'POST',
+            body: JSON.stringify(adminData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then($formAdmin.submit())
     }
 
-   fetch('/create', {
-    method: 'POST', 
-    body: JSON.stringify(adminData),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-   }).then($formAdmin.submit())
-    }
-    
 }
 
 
@@ -82,4 +86,4 @@ function formDataToObj(FormData) {
     const FormDataObj = {};
     FormData.forEach((value, key) => FormDataObj[key] = value);
     return FormDataObj
-  }
+}
